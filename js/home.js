b8ec8f79d44d5f66b624c8ca268a4ef5acba7f0f
Lukas Reschke <lukas@statuscode.ch>
2016-08-31 20:36:02 +0200

    Add animations

---
+++ js/home.js
@@ -0,0 +1,22 @@
+$(window).load(function() {
+
+    var waypoint = new Waypoint({
+        element: document.getElementById('get-nextcloud-button'),
+        handler: function(direction) {
+          anim.play();
+        }
+    });
+
+    var animContainer = document.getElementById('bodymovin');
+    var params = {
+        container: animContainer,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        autoloadSegments: false,
+        path: '../img/home/community/data.json'// path to your data.json file you rendered from AE
+
+    };
+    var anim;
+    anim = bodymovin.loadAnimation(params);
+});
