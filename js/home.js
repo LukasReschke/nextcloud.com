4936fb6c7b837158053aa1130c6df96877ea24ae
Lukas Reschke <lukas@statuscode.ch>
2016-09-01 11:04:41 +0200

    White nav bar when scrolling

--- js/home.js
+++ js/home.js
@@ -1,12 +1,34 @@
 $(window).load(function() {
+    /**
+     * Listen to scroll to change header opacity class
+     */
+    function checkScroll(){
+        var startY = $('.navbar').height() * 2;
 
+        if($(window).scrollTop() > startY){
+            $('.navbar img').attr('src', '../img/logo_nextcloud_blue.svg');
+            $('.navbar').addClass('scrolled');
+        }else{
+            $('.navbar img').attr('src', '../img/logo_nextcloud_white.svg');
+            $('.navbar').removeClass('scrolled');
+        }
+    }
+
+    if($('.navbar').length > 0){
+        $(window).on('scroll load resize', function(){
+            checkScroll();
+        });
+    }
+
+    /**
+     * Animation
+     */
     var waypoint = new Waypoint({
         element: document.getElementById('get-nextcloud-button'),
         handler: function(direction) {
           anim.play();
         }
     });
-
     var animContainer = document.getElementById('bodymovin');
     var params = {
         container: animContainer,
