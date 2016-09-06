8e1b3bc82690351144cd212b179f605fdcf0c8a3
Lukas Reschke <lukas@statuscode.ch>
2016-09-06 22:22:17 +0200

    Use animations for all

--- js/home.js
+++ js/home.js
@@ -19,24 +19,31 @@ $(window).load(function() {
     }
 
     /**
-     * Animation
+     * Animations
      */
-    var waypoint = new Waypoint({
-        element: document.getElementById('get-nextcloud-button'),
-        handler: function(direction) {
-          anim.play();
-        }
-    });
-    var animContainer = document.getElementById('bodymovin');
-    var params = {
-        container: animContainer,
-        renderer: 'svg',
-        loop: false,
-        autoplay: false,
-        autoloadSegments: false,
-        path: 'img/home/community/data.json'// path to your data.json file you rendered from AE
+    var elements = [
+        'community',
+        'privacy',
+        'flexibility'
+    ];
+    elements.forEach(function(key) {
+        new Waypoint({
+            element: document.getElementById('get-nextcloud-button'),
+            handler: function() {
+                anim.play();
+            }
+        });
+        var animContainer = document.querySelectorAll('#why-nextcloud div [data-name='+key+']')[0];
+        var params = {
+            container: animContainer,
+            renderer: 'svg',
+            loop: false,
+            autoplay: false,
+            autoloadSegments: false,
+            path: 'img/home/'+key+'/data.json'// path to your data.json file you rendered from AE
 
-    };
-    var anim;
-    anim = bodymovin.loadAnimation(params);
+        };
+        var anim;
+        anim = bodymovin.loadAnimation(params);
+    });
 });
